b003ce99187bf797d94f80119b7365310686944c
toloudis <danielt@alleninstitute.org>
2024-04-08 08:19:37 -0700

    clean up init.py

--- nbvv/__init__.py
+++ nbvv/__init__.py
@@ -1,5 +1,3 @@
-from .volume import *
-
 try:
     from ._version import __version__
 except ImportError:
@@ -7,21 +5,19 @@ except ImportError:
     # in editable mode with pip. It is highly recommended to install
     # the package from a stable release or in editable mode: https://pip.pypa.io/en/stable/topics/local-project-installs/#editable-installs
     import warnings
+
     warnings.warn("Importing 'nbvv' outside a proper installation.")
     __version__ = "dev"
 
+from .volume import *
+
 
 def _jupyter_labextension_paths():
-    return [{
-        "src": "labextension",
-        "dest": "nbvv"
-    }]
+    return [{"src": "labextension", "dest": "nbvv"}]
 
 
 def _jupyter_server_extension_points():
-    return [{
-        "module": "nbvv"
-    }]
+    return [{"module": "nbvv"}]
 
 
 def _load_jupyter_server_extension(server_app):
